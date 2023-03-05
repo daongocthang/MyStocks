ea7cb1a46f394c68416850e9804e31e63df7155b
wayfar3 <daongocthang.vn@gmail.com>
2023-03-06 06:17:24 +0700

    update

--- app/src/main/java/com/standalone/mystocks/handlers/generic/DatabaseManager.java
+++ app/src/main/java/com/standalone/mystocks/handlers/generic/DatabaseManager.java
@@ -3,17 +3,21 @@ package com.standalone.mystocks.handlers.generic;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
+import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Properties;
 
 public class DatabaseManager {
-
     @SuppressLint("StaticFieldLeak")
     private static OpenDB sqliteOpenInstance;
 
-    private static OpenDB getSqliteOpenInstance(Context context) {
+    public static SQLiteDatabase getDatabase(Context context) {
         if (sqliteOpenInstance == null) {
             try {
                 sqliteOpenInstance = new OpenDB(context, getProperty(context, "database_name"), Integer.parseInt(getProperty(context, "database_version")));
@@ -21,20 +25,67 @@ public class DatabaseManager {
                 e.printStackTrace();
             }
         }
-
-        return sqliteOpenInstance;
-    }
-
-    public static SQLiteDatabase getDatabase(Context context) {
-        return getSqliteOpenInstance(context).getDatabase();
+        return sqliteOpenInstance.getDatabase();
     }
 
     public static void importDatabase(Context context, String filename) {
-        getSqliteOpenInstance(context).restore(filename);
+        try {
+            //database path
+            final String databasePath = context.getDatabasePath(getProperty(context, "database_name")).toString();
+            File dbFile = new File(databasePath);
+            FileInputStream fis = new FileInputStream(dbFile);
+
+            // Open the empty db as the output stream
+            OutputStream output = new FileOutputStream(filename);
+
+            // Transfer bytes from the input file to the output file
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = fis.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+
+            // Close the streams
+            output.flush();
+            output.close();
+            fis.close();
+
+            Toast.makeText(context, "Backup Completed", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Toast.makeText(context, "Unable to backup database. Retry", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 
     public static void exportDatabase(Context context, String filename) {
-        getSqliteOpenInstance(context).backup(filename);
+        try {
+            //database path
+            final String databasePath = context.getDatabasePath(getProperty(context, "database_name")).toString();
+            File dbFile = new File(filename);
+            FileInputStream fis = new FileInputStream(dbFile);
+
+            // Open the empty db as the output stream
+            OutputStream output = new FileOutputStream(databasePath);
+
+            // Transfer bytes from the input file to the output file
+            byte[] buffer = new byte[1024];
+            int length;
+            while ((length = fis.read(buffer)) > 0) {
+                output.write(buffer, 0, length);
+            }
+
+            // Close the streams
+            output.flush();
+            output.close();
+            fis.close();
+
+            Toast.makeText(context, "Import Completed", Toast.LENGTH_SHORT).show();
+
+        } catch (Exception e) {
+            Toast.makeText(context, "Unable to import database. Retry", Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
     }
 
     private static String getProperty(Context context, String key) throws IOException {
